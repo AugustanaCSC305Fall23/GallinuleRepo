ab462f3129f46bd8b3cfe63754913fbe52d6995f
Mason <qcmason648@gmail.com>
2023-10-09 17:55:46 -0500

    Tertiary screen added (create plan screen) w/ function to go back to the secondary screen

--- GallinuleProject/src/main/java/edu/augustana/TertiaryController.java
+++ GallinuleProject/src/main/java/edu/augustana/TertiaryController.java
@@ -6,7 +6,7 @@ import javafx.fxml.FXML;
 public class TertiaryController {
 
     @FXML
-    private void switchToPrimary() throws IOException {
-        App.setRoot("primary");
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
     }
 }
