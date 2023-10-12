bd3ae20136db4044fb12f297e6055d24d400167f
Mason <qcmason648@gmail.com>
2023-10-12 12:53:02 -0500

    Changed the class and fxml names to make sense to it's functionTAKE NOTE BEFORE WRITING MORE CODE FOR SAID CLASSES <3

---
+++ GallinuleProject/src/main/java/edu/augustana/GenderController.java
@@ -0,0 +1,33 @@
+package edu.augustana;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class GenderController {
+
+    @FXML
+    private void switchToWelcome() throws IOException {
+        App.setRoot("Welcome");
+    }
+
+    @FXML
+    private void handleMaleButtonClick() throws IOException {
+        // Handle the "Male" button click here
+        // You can change the behavior as needed
+        App.setRoot("Tertiary");
+    }
+
+    @FXML
+    private void handleFemaleButtonClick() throws IOException {
+        // Handle the "Female" button click here
+        // You can change the behavior as needed
+        App.setRoot("Tertiary");
+    }
+
+    @FXML
+    private void handleNeutralButtonClick() throws IOException {
+        // Handle the "Neutral" button click here
+        // You can change the behavior as needed
+        App.setRoot("Tertiary");
+    }
+}
--- GallinuleProject/src/main/java/edu/augustana/WelcomeController.java
+++ GallinuleProject/src/main/java/edu/augustana/WelcomeController.java
@@ -3,10 +3,17 @@ package edu.augustana;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-public class TertiaryController {
+public class WelcomeController {
 
     @FXML
     private void switchToGender() throws IOException {
         App.setRoot("Gender");
     }
+
+
+    @FXML
+    private void switchToBrowsePlan() throws IOException {
+        App.setRoot("BrowsePlan");
+    }
+
 }
