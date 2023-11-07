063618f46bfa04c9136fdc617d623f88ccbca891
Aarshiya <aarshiyaprajapati@gmail.com>
2023-11-07 10:04:27 -0600

    Completed the Course class with save and load methods

--- GallinuleProject/src/main/java/edu/augustana/Course.java
+++ GallinuleProject/src/main/java/edu/augustana/Course.java
@@ -1,10 +1,10 @@
 package edu.augustana;
 
 
-import java.util.List;
 import java.io.*;
+import java.util.List;
 
-public class Course {
+public class Course implements Serializable {
     private String title ;
     private List<LessonPlan> lessons;
 
@@ -34,6 +34,32 @@ public class Course {
     }
 
 
+    public static void saveCourse(Course course) {
+        String fileName = course.getTitle()+".gymCourse";
+        try (FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)) {
+            objectOutputStream.writeObject(course);
+            System.out.println("Course saved to " + fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Error saving the course to " + fileName);
+        }
+    }
+
+    public static Course loadCourse(String fileName){
+        Course course = null;
+        try{
+            ObjectInputStream input = new ObjectInputStream(new FileInputStream((fileName)));
+            course = (Course) input.readObject();
+
+        } catch(IOException e){
+            System.err.println("Error opening the course " + fileName);
+        } catch(ClassNotFoundException cnfe){
+            System.err.println("Object read is not a Course");
+
+        }
+        return course;
+    }
 
 }
 
