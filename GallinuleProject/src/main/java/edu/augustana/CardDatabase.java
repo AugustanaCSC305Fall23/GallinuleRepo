5e42d61c3f19d3c32dd8d80f4b0585d9f4ca9688
StephenKodua <koduastephen9@gmail.com>
2023-11-17 23:37:58 -0600

    Refractored CardDataBase class

--- GallinuleProject/src/main/java/edu/augustana/CardDatabase.java
+++ GallinuleProject/src/main/java/edu/augustana/CardDatabase.java
@@ -13,37 +13,44 @@ import java.util.List;
 import java.util.Map;
 
 public class CardDatabase {
-    private static final List<Card> allCards = new ArrayList<>();
-    private static Map<String,Card> allCardsMap;
+    static final List<Card> allCards = new ArrayList<>();
+    static final Map<String,Card> allCardsMap = new HashMap<>(); //declaration to avoid null references
 
-    public static List<Card> getAllCards() {
-        return allCards;
-    }
 
     public static void addCardsFromCSVFile(File csvFile) throws CsvValidationException, IOException {
-        CSVReader reader = new CSVReaderBuilder(new FileReader(csvFile)).build();
-        String[] nextLine;
-        allCardsMap = new HashMap<>();
-        boolean isFirstLine = true;
-        while ((nextLine = reader.readNext()) != null) {
-            if (isFirstLine) {
-                isFirstLine = false;
-            } else {
-                Card card = new Card(nextLine);
-                allCardsMap.put(card.getCode(), card);
-                allCards.add(card);
+        //Use try for CSVReader to ensure close after reading
+        try (CSVReader reader = new CSVReaderBuilder(new FileReader(csvFile)).build()) {
+            String[] nextLine;
+            boolean isFirstLine = true;
+            while ((nextLine = reader.readNext()) != null) {
+                if (isFirstLine) {
+                    isFirstLine = false;
+                } else {
+                    Card card = new Card(nextLine);
+                    allCardsMap.put(card.getCode(), card);
+                    allCards.add(card);
+                }
             }
         }
+    }
+
+
+    public static Card getCardByID(String cardID) {
+        return allCardsMap.get(cardID);
+    }
+
+    public static List<Card> getAllCards() {
+        return new ArrayList<>(allCards);
+    }
+
+    public static Map<String, Card> getAllCardsMap() {
+        return new HashMap<>(allCardsMap);
     }
 
     public static void main(String[] args) throws CsvValidationException, IOException {
         File csvFile = new File("src/main/resources/DEMO1.csv");
         addCardsFromCSVFile(csvFile);
 
-    }
-
-    public static Card getCardByID(String cardID) {
-        return allCardsMap.get(cardID);
     }
 
 }
