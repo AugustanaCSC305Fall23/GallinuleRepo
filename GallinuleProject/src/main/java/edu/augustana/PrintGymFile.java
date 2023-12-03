c7769c8066db56eb5e5cbd3b680d8427e8381036
StephenKodua <koduastephen9@gmail.com>
2023-12-03 02:08:13 -0600

    Added A4 sheet scale to print

--- GallinuleProject/src/main/java/edu/augustana/PrintGymFile.java
+++ GallinuleProject/src/main/java/edu/augustana/PrintGymFile.java
@@ -1,34 +1,48 @@
 package edu.augustana;
 
 import javafx.print.*;
-import javafx.scene.Node;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
 
 public class PrintGymFile {
 
-
-    public void printFile(AnchorPane AnchorPane) {
-
+    public void printFile(AnchorPane anchorPane) {
 
         PrinterJob job = PrinterJob.createPrinterJob(Printer.getDefaultPrinter());
 
         if (job != null) {
             boolean showDialog = job.showPrintDialog(null);
-            PageLayout pageLayout = job.getPrinter().createPageLayout(Paper.A5, PageOrientation.LANDSCAPE, 0, 0, 0, 0);
+
             if (showDialog) {
-                boolean success = job.printPage(pageLayout, AnchorPane);
+                PageLayout pageLayout = job.getPrinter().createPageLayout(Paper.A4, PageOrientation.LANDSCAPE, Printer.MarginType.HARDWARE_MINIMUM);
+
+                // Create a copy of the content to avoid modifying the original
+                AnchorPane scaledContent = createScaledContent(anchorPane, pageLayout);
+
+                boolean success = job.printPage(pageLayout, scaledContent);
 
                 if (success) {
                     job.endJob();
                 }
             }
         } else {
-
             System.out.println("There is no printer connected");
         }
     }
 
+    // Create a scaled copy of the content to fit on A4
+    private AnchorPane createScaledContent(AnchorPane content, PageLayout pageLayout) {
+        double scaleFactor = calculateScaleFactor(content, pageLayout);
+        AnchorPane scaledContent = new AnchorPane(content.getChildren().toArray(new javafx.scene.Node[0]));
+        scaledContent.getTransforms().add(new javafx.scene.transform.Scale(scaleFactor, scaleFactor));
+        return scaledContent;
+    }
+
+    // Calculate the scale factor to fit the content on A4
+    private double calculateScaleFactor(AnchorPane content, PageLayout pageLayout) {
+        double scaleX = pageLayout.getPrintableWidth() / content.getBoundsInParent().getWidth();
+        double scaleY = pageLayout.getPrintableHeight() / content.getBoundsInParent().getHeight();
+
+        // Use the minimum scale factor to maintain aspect ratio
+        return Math.max(scaleX, scaleY);
+    }
 }
