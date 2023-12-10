f556adab18df14f0743df600701029f099ff867c
StephenKodua <koduastephen9@gmail.com>
2023-12-10 02:26:44 -0600

    Add javadoc to FavoriteSet

--- GallinuleProject/src/main/java/edu/augustana/GenderController.java
+++ GallinuleProject/src/main/java/edu/augustana/GenderController.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 
 import javafx.fxml.FXML;
 
+/**
+ * Controller class for the gender selection screen.
+ * Manages the navigation to the welcome screen and handles button clicks for gender selection.
+ */
 public class GenderController {
 
     @FXML
@@ -14,21 +18,18 @@ public class GenderController {
     @FXML
     private void handleMaleButtonClick() throws IOException {
         // Handle the "Male" button click here
-        // You can change the behavior as needed
         App.setRoot("Start");
     }
 
     @FXML
     private void handleFemaleButtonClick() throws IOException {
         // Handle the "Female" button click here
-        // You can change the behavior as needed
         App.setRoot("Start");
     }
 
     @FXML
     private void handleNeutralButtonClick() throws IOException {
         // Handle the "Neutral" button click here
-        // You can change the behavior as needed
         App.setRoot("Start");
     }
 }
