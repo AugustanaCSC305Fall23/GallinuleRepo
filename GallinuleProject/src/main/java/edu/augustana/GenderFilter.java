8eb36255728c9631e05778ce77b5c0c09a599852
StephenKodua <koduastephen9@gmail.com>
2023-12-10 01:30:18 -0600

    Add javadoc to GenderFilter

--- GallinuleProject/src/main/java/edu/augustana/GenderFilter.java
+++ GallinuleProject/src/main/java/edu/augustana/GenderFilter.java
@@ -3,14 +3,28 @@ package edu.augustana;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Implementation of CardFilter for filtering cards based on a specific gender.
+ */
 public class GenderFilter implements CardFilter {
 
     private final String selectedGender;
 
+    /**
+     * Constructs a GenderFilter with the specified gender.
+     *
+     * @param selectedGender The gender to filter by.
+     */
     public GenderFilter(String selectedGender) {
         this.selectedGender = selectedGender;
     }
 
+    /**
+     * Maps the selected gender to its corresponding code character.
+     *
+     * @param selectedGender The selected gender.
+     * @return The code character corresponding to the selected gender.
+     */
     private String getGenderCodeChar(String selectedGender) {
 
         if ("Male".equals(selectedGender)) {
@@ -24,6 +38,12 @@ public class GenderFilter implements CardFilter {
         }
     }
 
+    /**
+     * Filters the list of cards based on the selected gender.
+     *
+     * @param cards The list of cards to be filtered.
+     * @return The filtered list of cards.
+     */
     @Override
     public List<Card> filter(List<Card> cards) {
         return filterByGender(cards);
