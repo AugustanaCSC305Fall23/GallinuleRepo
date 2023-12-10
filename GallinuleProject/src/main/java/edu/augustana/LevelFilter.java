adf3b1fb9d5b3643996c1bbecd37f221bb98dd83
StephenKodua <koduastephen9@gmail.com>
2023-12-10 01:32:28 -0600

    Add javadoc to LevelFilter

--- GallinuleProject/src/main/java/edu/augustana/LevelFilter.java
+++ GallinuleProject/src/main/java/edu/augustana/LevelFilter.java
@@ -5,6 +5,9 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+/**
+ * Implementation of CardFilter for filtering cards based on a specific level.
+ */
 public class LevelFilter implements CardFilter {
     private final String selectedLevel;
     private static final Map<String, String> levelMapping = Map.of(
@@ -13,19 +16,33 @@ public class LevelFilter implements CardFilter {
             "Advanced Beginner", "AB",
             "Intermediate", "I",
             "Advanced", "A"
-    );;
+    );
+
+    /**
+     * Constructs a LevelFilter with the specified level.
+     *
+     * @param selectedLevel The level to filter by.
+     */
 
     public LevelFilter(String selectedLevel) {
-
-        this.selectedLevel = selectedLevel ;
-
-
+        this.selectedLevel = selectedLevel;
     }
 
+    /**
+     * Gets the full names of all available levels.
+     *
+     * @return A set of full level names.
+     */
     public static Set<String> getFullLevelNames() {
         return levelMapping.keySet();
     }
 
+    /**
+     * Filters the list of cards based on the selected level.
+     *
+     * @param cards The list of cards to be filtered.
+     * @return The filtered list of cards.
+     */
     @Override
     public List<Card> filter(List<Card> cards) {
         return filterByLevel(cards);
@@ -37,21 +54,19 @@ public class LevelFilter implements CardFilter {
             return cards.stream()
                     .filter(card -> matchesAnyLevel(card.getLevels()))
                     .collect(Collectors.toList());
-        }else{
+        } else {
             return cards;
         }
     }
 
     private boolean matchesAnyLevel(List<String> cardLevels) {
 
-            for (String cardLevel : cardLevels) {
-                if (cardLevel.contains(levelMapping.get(selectedLevel))) {
-                    return true;
-                }
+        for (String cardLevel : cardLevels) {
+            if (cardLevel.contains(levelMapping.get(selectedLevel))) {
+                return true;
             }
-
+        }
         return false;
     }
-
-
+    
 }
